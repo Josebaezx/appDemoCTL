a193489ba4038869af8b3c198e93ee00f302b0df
Josebaezx <jose_baez84@hotmail.com>
2023-02-21 19:51:52 -0300

    primer commit

---
+++ src/main/java/com/ctl/prueba/AppDemoCtlApplication.java
@@ -0,0 +1,13 @@
+package com.ctl.prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemoCtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDemoCtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctl/prueba/AppDemoCtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctl.prueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemoCtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
